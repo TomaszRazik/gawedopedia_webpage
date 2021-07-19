561a06036566bf50a8fc84fbce355157354e5cb9
Tomasz Razik <tomasz.kamil.razik@gmail.com>
2021-07-19 19:38:34 +0200

    Commit

--- frontend/src/components/FrontendUtils.js
+++ frontend/src/components/FrontendUtils.js
@@ -2,6 +2,13 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/core/styles";
 import { yellow } from "@material-ui/core/colors";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
 
 export const YellowButton = withStyles((theme) => ({
   root: {
@@ -21,6 +28,61 @@ export function SuccessSnackbar(message) {
           {message}
         </Alert>
       </Snackbar>
+    </div>
+  );
+}
+// Poniższe funncje nie działają poprawnie. Naprawić.
+export function DeleteWindow(isOpen, onClose) {
+  return (
+    <div>
+      <Dialog
+        open={isOpen}
+        onClose={onClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Czy na pewno chcesz usunąć ten element?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Ta operacja nie będzie mogła być cofnięta. Wpis zostanie trwale
+            usunięty.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cofnij</Button>
+          <Button variant="contained" color="secondary">
+            Usuń
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export function EditLandWindow(isOpen, onClose) {
+  return (
+    <div>
+      <Dialog
+        open={isOpen}
+        onClose={onClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Edycja pozycji"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Tu będzie forma z możliwością zmiany zapisanych danych.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cofnij</Button>
+          <Button variant="contained" color="primary">
+            Zapisz
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
